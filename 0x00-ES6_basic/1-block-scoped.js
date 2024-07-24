bcd67b6efc3799f9ddf73feb315471df605b3766
kassem00 <kamsdonga@gmail.com>
2024-07-24 15:26:06 +0000

    au

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const newTask = true;
-    const newTask2 = false;
-    return [newTask, newTask2];
+    const task = true;
+    const task2 = false;
+    return [task, task2];
   }
   return [task, task2];
 }
