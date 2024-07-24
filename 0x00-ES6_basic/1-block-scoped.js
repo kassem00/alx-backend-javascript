a6874932e166b89a246d5ebcd578a9efd5ce4cb5
kassem00 <kamsdonga@gmail.com>
2024-07-24 16:35:10 +0000

    au

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,7 +5,6 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
     const task2 = false; // eslint-disable-line no-unused-vars
-    return [task, task2];
   }
   return [task, task2];
 }
