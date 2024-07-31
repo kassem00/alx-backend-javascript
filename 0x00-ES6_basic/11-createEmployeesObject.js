1e15b7f7f38a0bdd8f95925086c9ac23899dd92d
kassem00 <kamsdonga@gmail.com>
2024-07-31 21:38:26 +0000

    au

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const Employees = new Object()
-  Employees.departmentName = departmentName;
-  Employees.employees = employees;
-  return {[Employees.departmentName]: Employees.employees};
+  return {
+    [departmentName]: employees
+  };
 }
