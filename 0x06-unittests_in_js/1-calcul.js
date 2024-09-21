0443d88b77b8dfaa41872aaa5076102a0a333855
kassem00 <kamsdonga@gmail.com>
2024-09-21 23:15:20 +0000

    au

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,16 +1,18 @@
-// 0-calcul.js
+// 1-calcul.js
 
 function calculateNumber(type, a, b) {
-    switch (type){
-
-    case 'SUM':
-	return Math.round(a) + Math.round(b);
-    case "SUBTRACT":
-	return Math.round(a) - Math.round(b);
-    case "DIVIDE":
-	return Math.round(a) / Math.round(b);
-    default:
-	return "Error";
+    switch (type) {
+        case 'SUM':
+            return Math.round(a) + Math.round(b);
+        case 'SUBTRACT':
+            return Math.round(a) - Math.round(b);
+        case 'DIVIDE':
+            if (Math.round(b) === 0) {
+                return "Error";
+            }
+            return Math.round(a) / Math.round(b);
+        default:
+            return "Error";
     }
 }
 
