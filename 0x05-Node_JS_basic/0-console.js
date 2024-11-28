f0b6d082e2587332f740af37365eaaa8e4c4fe02
kassem00 <kamsdonga@gmail.com>
2024-11-28 04:27:40 +0000

    au

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
 const displayMessage = (data) => {
-  process.stdout.write(`${data}` + "\n");
+  process.stdout.write(`${data}`);
+  process.stdout.write('\n');
 };
 
 module.exports = displayMessage;
