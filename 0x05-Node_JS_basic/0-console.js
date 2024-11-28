32e9202dd60577bff815fb4329e42df1658604b2
K_Lab <kassem@localhost.localdomain>
2024-11-28 17:52:36 +0200

    au

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,17 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
 const displayMessage = (data) => {
   process.stdout.write(`${data}`);
   process.stdout.write('\n');
 };
 
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
 module.exports = displayMessage;
