556877396961bf710e0fd9ec73b5080dd50e1f0f
K_Lab <kassem@localhost.localdomain>
2024-11-29 17:03:22 +0200

    au

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,12 @@
 export default function createIteratorObject(report) {
-
+    let ele = [];
+    for (const department of Object.keys(report.allEmployees)) {
+        for (let nu = 0; nu < report.allEmployees[department].length; nu++) {
+            ele.push(report.allEmployees[department][nu]);
+        }
+    }
+    
+  return ele;
 }
+
+
