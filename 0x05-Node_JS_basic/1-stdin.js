f30733e0edd8603ecd348d1b5b8913191d3d3030
K_Lab <kassem@localhost.localdomain>
2024-11-29 07:13:47 +0200

    au

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const userName = data.toString().trim();
-  console.log(`Your name is: ${userName}`);
+  console.log(`Your name is: ${userName}\r`);
   process.exit();
 });
