d3f131259331ed97cc39c148e8af7860bbdb836c
K_Lab <kassem@localhost.localdomain>
2024-11-30 05:49:25 +0200

    au

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,19 +3,14 @@ const path = require('path');
 
 function countStudents(filePath) {
   const absolutePath = path.resolve(filePath);
-  console.log(`Checking if file exists at: ${absolutePath}`);
-
-  // Check if the file exists
   if (!fs.existsSync(absolutePath)) {
     console.log('File does not exist');
-    return; // Gracefully handle the missing file
+    return;
   }
 
-  // If file exists, try to read it synchronously
   try {
     const data = fs.readFileSync(absolutePath, 'utf-8');
 
-    // Split the data into rows
     const rows = data.split('\n').filter((row) => row.trim() !== '');
 
     if (rows.length === 0) {
@@ -23,13 +18,11 @@ function countStudents(filePath) {
       return;
     }
 
-    // Process CSV data
     const students = rows.slice(1).map((row) => {
       const [firstname, , , field] = row.split(','); // Removed lastname and age
       return { firstname, field };
     });
 
-    // Count students in each field
     const fieldCounts = students.reduce((acc, student) => {
       if (acc[student.field]) {
         acc[student.field].push(student.firstname);
@@ -39,13 +32,10 @@ function countStudents(filePath) {
       return acc;
     }, {});
 
-    // Display the total number of students
     const totalStudents = students.length;
     console.log(`Number of students: ${totalStudents}`);
 
-    // Display the number of students in each field
     for (const field in fieldCounts) {
-      // Check if field is a direct property (not from prototype)
       if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
         const fieldList = fieldCounts[field];
         console.log(`Number of students in ${field}: ${fieldList.length}. List: ${fieldList.join(', ')}`);
