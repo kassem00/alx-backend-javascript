29470ecaa39d38e607448e3d71b88ec8c20eb56e
kassem00 <kamsdonga@gmail.com>
2024-12-03 13:11:55 +0200

    au

--- 0x01-ES6_promise/.eslintrc.cjs
+++ 0x01-ES6_promise/.eslintrc.cjs
@@ -1,35 +1,13 @@
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
-};
+{
+  "extends": ["airbnb-base"],
+  "plugins": ["import"],
+  "rules": {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "always"
+      }
+    ]
+  }
+}
