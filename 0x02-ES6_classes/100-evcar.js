f2266c6f3c401ebacddaf0291155e314743b21cf
kassem00 <kamsdonga@gmail.com>
2025-02-13 12:41:22 +0200

    au

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -14,31 +14,13 @@ export default class EVCar extends Car {
     this._range = value;
   }
 
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
-  }
-}
-import Car from './10-car';
-
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this.range = range;
-  }
-
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
+  // Override Symbol.species to ensure cloning returns EVCar instead of Car
+  static get [Symbol.species]() {
+    return this; // Ensures cloning returns EVCar instances
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    const Species = this.constructor[Symbol.species]; // Ensures it uses EVCar[Symbol.species]
+    return new Species(this.brand, this.motor, this.color, this.range);
   }
 }
