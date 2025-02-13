1be0e6104181a3c1d762e1bd2bfa4dc2f97da68e
kassem00 <kamsdonga@gmail.com>
2025-02-13 11:24:21 +0200

    au

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,8 +1,3 @@
-//Fix this code and make it work.
-
-const class2019 = new ALXClass(2019, 'San Francisco');
-const class2020 = new ALXClass(2020, 'San Francisco');
-
 export class ALXClass {
   constructor(year, location) {
     this._year = year;
@@ -18,17 +13,11 @@ export class ALXClass {
   }
 }
 
-const student1 = new StudentALX('Guillaume', 'Salva', class2020);
-const student2 = new StudentALX('John', 'Doe', class2020);
-const student3 = new StudentALX('Albert', 'Clinton', class2019);
-const student4 = new StudentALX('Donald', 'Bush', class2019);
-const student5 = new StudentALX('Jason', 'Sandler', class2019);
-
 export class StudentALX {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._alxClass = alxClass;
+    this._alxClass = alxClass; // Fixing the missing parameter
   }
 
   get fullName() {
@@ -36,10 +25,26 @@ export class StudentALX {
   }
 
   get alxClass() {
-    return this.alxClass;
+    return this._alxClass; // Fixing incorrect getter
   }
 
   get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._alxClass.year} - ${self._alxClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
+
+// Creating ALXClass instances
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+
+// Creating StudentALX instances
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
+
+// Creating the list of students
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
