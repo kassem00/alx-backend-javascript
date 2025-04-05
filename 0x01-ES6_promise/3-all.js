76c6dea53c0622a53b35157c4cc23db7869ba210
kassem00 <kamsdonga@gmail.com>
2025-04-05 15:43:05 +0200

    au

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   try {
-    // Wait for both promises to resolve
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-
-    // Log the expected output
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (error) {
-    // Handle errors and log "Signup system offline"
     console.log('Signup system offline');
   }
 }
